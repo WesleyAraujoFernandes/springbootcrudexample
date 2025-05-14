807d45898f057907586473ea4e12c73767cd7efe
WesleyAraujoFernandes <wesley@viaapia.com.br>
2025-05-14 16:19:46 -0300

    Commit inicial

---
+++ src/test/java/com/example/ctpseducation/springbootcrudexample/SpringbootcrudexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ctpseducation.springbootcrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcrudexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
